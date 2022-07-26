30d98e1bce89eeb35b79d93d82bc7d687be834ec
Maria Ambrosimova <ambrosimova@rambler.ru>
2022-07-26 10:14:55 +0300

    renew brain-games.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
 
 console.log ("Welcome to the Brain Games!");
-const name = readlineSync.question('May I have your name? ');
+import {name} from '../src/cli.js';
 console.log('Hello, ' + name + '!');
