8b5fa978f8d3da6f80d3665921e3c40fea141ec8
Maria Ambrosimova <ambrosimova@rambler.ru>
2022-07-27 16:27:03 +0300

    add brain-even

---
+++ bin/brain-even.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+
+console.log ("Welcome to the Brain Games!");
+import {name} from '../src/brain-even_1game.js';
+console.log('Hello, ' + name + '!');
+console.log ('Answer "yes" if the number is even, otherwise answer "no".');
---
+++ src/brain-even_1game.js
@@ -0,0 +1,7 @@
+import readlineSync from 'readline-sync';
+//import Math.random() from 'Math.js';// нужно ли подключать встроенную библиотеку MAth?
+
+
+export const name = readlineSync.question('May I have your name? ');
+export let RN= Math.RandomNumber();
+export const RandomNumber= readlineSync.question('Question'+':'+'RN');
