b8c7458733425097255230b23efa70c1b5c1d557
Maria Ambrosimova <ambrosimova@rambler.ru>
2022-07-29 12:39:11 +0300

    try of 1st game

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,6 +1,17 @@
 #!/usr/bin/env node
 
-console.log ("Welcome to the Brain Games!");
-import {name} from '../src/brain-even_1game.js';
-console.log('Hello, ' + name + '!');
+//console.log ("Welcome to the Brain Games!");
+
+import {Greating} from '../src/cli.js';
+import {RN,RandomQuestion, check} from '../src/brain-even_1game.js';
+
+console.log (Greating());
 console.log ('Answer "yes" if the number is even, otherwise answer "no".');
+//console.log (RandomQuestion(RN));
+
+console.log(check(RN,RandomQuestion));
+
+
+//console.log('Your answer ' +':'+ RandomNumberQuestion );
+//check(RN, RandomNumberQuestion);
+
--- src/brain-even_1game.js
+++ src/brain-even_1game.js
@@ -1,7 +1,37 @@
 import readlineSync from 'readline-sync';
-//import Math.random() from 'Math.js';// нужно ли подключать встроенную библиотеку MAth?
+import {Greating} from '../src/cli.js';
 
+//import Math.random from './Math';// нужно ли подключать встроенную библиотеку MAth?
 
-export const name = readlineSync.question('May I have your name? ');
-export let RN= Math.RandomNumber();
-export const RandomNumber= readlineSync.question('Question'+':'+'RN');
+//задание рандомного числа
+export let RN = Math.round(Math.random()*100);
+
+//задание вопрос с рандомным числом и ответом да или нет
+export const RandomQuestion= (RN) => {
+    const Rquestion= readlineSync.question('Question'+':'+RN);
+    console.log('Your answer ' +':'+ Rquestion );
+    }
+
+// пишу функцию check, которая должна сравнивать ответ и RN и говорить, правда или нет.
+export const check = (RN, RandomQuestion) =>{
+    let n=2;// кол-во попыток
+    for (let i=0 ; i <=n; i++) {
+        let RN = Math.round(Math.random()*100);
+        console.log (RandomQuestion(RN));
+    if (RN % 2 == 0 && RandomQuestion ==='yes' || RN % 2 !== 0 && RandomQuestion ==='no') {
+        console.log('Correct!');
+
+    }    else{
+            let opposite;
+                if (RandomQuestion === 'yes'){
+                opposite === 'no';
+                } else {
+                opposite ==='yes';
+                }
+    console.log (RandomQuestion +'is wrong answer ;(. Correct answer was'+opposite);
+    break ;
+    }
+
+}//end cycle
+//console.log ('Congratulations,'+ name + '!');
+}//end check function
