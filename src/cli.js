478b0a3144f1768db1ed83446313f7617a80a433
Maria Ambrosimova <ambrosimova@rambler.ru>
2022-07-29 12:38:22 +0300

    right cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,3 +1,8 @@
 import readlineSync from 'readline-sync';
-
-export const name = readlineSync.question('May I have your name? ');
+// приветствие
+export const Greating= () => {
+    console.log ("Welcome to the Brain Games!");
+    const name = readlineSync.question('May I have your name? ');
+    console.log('Hello, ' + name + '!');
+}
+// вопрос про число
