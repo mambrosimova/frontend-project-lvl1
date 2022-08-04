0c7262f839e3070fd92b1f82675a4f4d18559bf6
Maria Ambrosimova <ambrosimova@rambler.ru>
2022-08-04 10:33:29 +0300

    brain-even

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,17 +1,4 @@
 #!/usr/bin/env node
 
-//console.log ("Welcome to the Brain Games!");
-
-import {Greating} from '../src/cli.js';
-import {RN,RandomQuestion, check} from '../src/brain-even_1game.js';
-
-console.log (Greating());
-console.log ('Answer "yes" if the number is even, otherwise answer "no".');
-//console.log (RandomQuestion(RN));
-
-console.log(check(RN,RandomQuestion));
-
-
-//console.log('Your answer ' +':'+ RandomNumberQuestion );
-//check(RN, RandomNumberQuestion);
-
+import {even} from '../src/brain-even_1game.js';
+ even ();
--- src/brain-even_1game.js
+++ src/brain-even_1game.js
@@ -1,37 +1,92 @@
 import readlineSync from 'readline-sync';
-import {Greating} from '../src/cli.js';
+// приветствие
 
-//import Math.random from './Math';// нужно ли подключать встроенную библиотеку MAth?
+export  const even= () =>{
+    // приветствие
+    console.log ("Welcome to the Brain Games!");
+    //знакомство
+    const name = readlineSync.question('May I have your name? ');
+    console.log('Hello, ' + name + '!');
+    //правила игры
+    console.log ('Answer "yes" if the number is even, otherwise answer "no".');
+
+    //цикл игры:    
+    // получить вопрос и ответ правильный ( 2 перемнных)
+    //задать вопрос и получить ответ игрока
+    //сравнение ответов 
+    let n=2;//кол-во попыток 3
+    for (let i=0; i<=n ; i++) {
+        let RN = Math.round(Math.random()*100);// задание рандомного числа -вопрос получили
+        const rightanswer = (RN)=>{// функция возвращает да или нет
+            if ( RN % 2 === 0) {
+                return 'yes';
+            } 
+            if ( RN % 2 !== 0) {
+                return 'no';
+            }
+        }
+        console.log(rightanswer(RN));
+        //задали вопрос и получили ответ на него
+        console.log('Question'+':'+RN);
+        const answer = readlineSync.question('Your answer ' +':');
+        //сравнение ответов
+         
+        const check = (rightanswer, answer) =>{
+            if (rightanswer = answer) {
+                 console. log ( 'Correct!');
+            } else {
+                console. log (answer +' is wrong answer ;(. Correct answer was');
+            }
+
+        }
+        check(rightanswer, answer);
+
+    }//end cycle 
+
+console.log ('Congratulations,'+ name + '!');
+
+}// end main function
+
+
 
 //задание рандомного числа
 export let RN = Math.round(Math.random()*100);
 
 //задание вопрос с рандомным числом и ответом да или нет
 export const RandomQuestion= (RN) => {
-    const Rquestion= readlineSync.question('Question'+':'+RN);
-    console.log('Your answer ' +':'+ Rquestion );
+    console.log('Question'+':'+RN);
+    const answer = readlineSync.question('Your answer ' +':');
     }
+
+//export const Rquestion === answer ;
 
 // пишу функцию check, которая должна сравнивать ответ и RN и говорить, правда или нет.
-export const check = (RN, RandomQuestion) =>{
-    let n=2;// кол-во попыток
-    for (let i=0 ; i <=n; i++) {
-        let RN = Math.round(Math.random()*100);
-        console.log (RandomQuestion(RN));
-    if (RN % 2 == 0 && RandomQuestion ==='yes' || RN % 2 !== 0 && RandomQuestion ==='no') {
-        console.log('Correct!');
+//export const check = (RN, Rquestion) =>{
+    //let n=2;// кол-во попыток
+   // for (let i=0 ; i <=n; i++) {
+    //if (RN % 2 == 0 && Rquestion ==='yes' || RN % 2 !== 0 && Rquestion ==='no') {
+        //console.log('Correct!');
+        //let RN = Math.round(Math.random()*100);
 
-    }    else{
-            let opposite;
-                if (RandomQuestion === 'yes'){
-                opposite === 'no';
-                } else {
-                opposite ==='yes';
-                }
-    console.log (RandomQuestion +'is wrong answer ;(. Correct answer was'+opposite);
-    break ;
-    }
+      // else{
+           // let opposite;
+               // if (RandomQuestion === 'yes'){
+                //opposite === 'no';
+                //} else {
+                //opposite ==='yes';
+                
+    //console.log (RandomQuestion +'is wrong answer ;(. Correct answer was'+opposite);
+    //break ;
+    //}
 
-}//end cycle
+//end cycle
 //console.log ('Congratulations,'+ name + '!');
-}//end check function
+//}//end check function
+//приветствие
+//знакомство
+//описаниеигры 
+//игровой цикл 
+    //получить вопрос и ответ правильный ( 2 перемнных)
+    //задать вопрос и получить ответ игрока
+    //сравнение ответов 
+    
