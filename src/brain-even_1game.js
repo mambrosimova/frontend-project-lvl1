bc038efd43abe87040631c80773ecdef68bbd4d7
Maria Ambrosimova <ambrosimova@rambler.ru>
2022-08-05 15:58:55 +0300

    fix 1st game

--- src/brain-even_1game.js
+++ src/brain-even_1game.js
@@ -17,29 +17,34 @@ export  const even= () =>{
     let n=2;//кол-во попыток 3
     for (let i=0; i<=n ; i++) {
         let RN = Math.round(Math.random()*100);// задание рандомного числа -вопрос получили
-        const rightanswer = (RN)=>{// функция возвращает да или нет
+        let rightanswer;
+        let wronganswer;
+        // функция возвращает да или нет
             if ( RN % 2 === 0) {
-                return 'yes';
+                rightanswer = 'yes';
+                wronganswer ='no';
             } 
             if ( RN % 2 !== 0) {
-                return 'no';
+                rightanswer = 'no';
+                wronganswer ='yes';
             }
-        }
-        console.log(rightanswer(RN));
+        //console.log (rightanswer);
+        //console.log (wronganswer);
+
         //задали вопрос и получили ответ на него
         console.log('Question'+':'+RN);
         const answer = readlineSync.question('Your answer ' +':');
         //сравнение ответов
          
-        const check = (rightanswer, answer) =>{
-            if (rightanswer = answer) {
+        const check = (answer) =>{
+            if (rightanswer === answer) {
                  console. log ( 'Correct!');
             } else {
-                console. log (answer +' is wrong answer ;(. Correct answer was');
+                console. log (answer +' is wrong answer ;(. Correct answer was' + rightanswer);
             }
 
         }
-        check(rightanswer, answer);
+        check(answer);
 
     }//end cycle 
 
@@ -49,35 +54,6 @@ console.log ('Congratulations,'+ name + '!');
 
 
 
-//задание рандомного числа
-export let RN = Math.round(Math.random()*100);
-
-//задание вопрос с рандомным числом и ответом да или нет
-export const RandomQuestion= (RN) => {
-    console.log('Question'+':'+RN);
-    const answer = readlineSync.question('Your answer ' +':');
-    }
-
-//export const Rquestion === answer ;
-
-// пишу функцию check, которая должна сравнивать ответ и RN и говорить, правда или нет.
-//export const check = (RN, Rquestion) =>{
-    //let n=2;// кол-во попыток
-   // for (let i=0 ; i <=n; i++) {
-    //if (RN % 2 == 0 && Rquestion ==='yes' || RN % 2 !== 0 && Rquestion ==='no') {
-        //console.log('Correct!');
-        //let RN = Math.round(Math.random()*100);
-
-      // else{
-           // let opposite;
-               // if (RandomQuestion === 'yes'){
-                //opposite === 'no';
-                //} else {
-                //opposite ==='yes';
-                
-    //console.log (RandomQuestion +'is wrong answer ;(. Correct answer was'+opposite);
-    //break ;
-    //}
 
 //end cycle
 //console.log ('Congratulations,'+ name + '!');
