0154cf131ff4a2eb1d1db54a14f140ef5c2d8627
Maria Ambrosimova <ambrosimova@rambler.ru>
2022-08-10 17:03:53 +0300

    4rth game with progression

--- src/index.js
+++ src/index.js
@@ -12,13 +12,20 @@ export const greeting= () =>{
     return name;
 }
 
-//задание рандомого числа
+//задание рандомого числа от 1-100
 export const randomnumber = ()=>{
 let a = Math.round(Math.random()*100);
 return a;
 }  
 
-//функция получения rightanswer
+//задание рандомого числа nго порядка
+export const randomnumber2 = (n)=>{//задать n=10 числа от 1-10, задать n=100 от 1-100
+    let a = Math.round(Math.random()*n);
+    return a;
+    }  
+
+
+//функция получения rightanswer в игре калькулятор
 export const getrightanswer= (n,m) => {
 const oper = ['+' , '-', '*'];//задание массива операций
 let RO = oper[Math.floor(Math.random() * (oper.length))];
@@ -35,7 +42,7 @@ let a;
     return a;
 }
 
-// функция получения мат операции для вопроса
+// функция получения мат операции для вопроса игра Калькулятор
 export const randomoper =(a,b,rightanswer) => {
     let result;
      if (rightanswer / b === a) {
@@ -66,5 +73,22 @@ for (let i=min; i>=1; i--) {
         return i;
     }
 }
+}
 
+export const getprogression =() => {
+    let start=randomnumber2(100);//задали начало
+    let step=randomnumber2(10);//задали шаг арифметич прогрессии
+    const result=[];// создали пустой массив
+    let resultlength=Math.floor(Math.random() * (10- 5 + 1)) + 5; //задание рандомной длины массива от 5-10 чисел
+ for (let i=0; i<=resultlength-1; i++) {
+    result.push(start+i*step);
+ }
+ return result;// заполнили массив
+
+}
+
+// ф-я , которая возвращает рандомный номер элемента массива
+export const randplacearray = (array) =>{
+let p = Math.floor(Math.random() * (array.length));
+return p;
 }
