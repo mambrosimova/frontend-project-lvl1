b786d6ec608d2be9ce134a4142e140801dc530d4
Maria Ambrosimova <ambrosimova@rambler.ru>
2022-08-21 22:52:48 +0300

    fix cal game

--- src/games/calc.js
+++ src/games/calc.js
@@ -2,16 +2,8 @@ import readlineSync from 'readline-sync';
 import {runLogic} from '../index.js';
 import {getRandomNumber} from '../utils.js';
 
-//нужно сгенерить массив из 3х массивов на 3 раунда [question, rightAnswer]
-const getRounds =()=> {
-    const rounds=[];//задали пустой массив для 3х раундов
-    const n=3;
-    for ( let i=1;i<=n; i++) {
-    const round=[];// свободный массив для одного раунда [question, rightAnswer]
-    let number1= getRandomNumber();
-    let number2=getRandomNumber();
-    
-    //ищем правильный ответ
+// получить правильный ответ
+const getRightAnswer = (number1, number2)=> {
     const arrayOper = ['+' , '-', '*'];//задание массива операций
     let randomOper= arrayOper[Math.floor(Math.random() * (arrayOper.length))];
     let rightAnswer;
@@ -24,19 +16,39 @@ const getRounds =()=> {
         if (randomOper === '*'){
             rightAnswer= number1*number2;
         }
+return rightAnswer;
+
+}
+
+//получить мат операцию для вопроса
+ const getOper= (rightAnswer, number1, number2) => {
+    let oper;
+    if (rightAnswer / number2 === number1) {
+       oper ='*';
+    }
+    if (rightAnswer-number2 === number1) {
+      oper ='+';
+    }
+    if (rightAnswer+number2 === number1) {
+       oper='-';
+    }
+return oper;
+
+ }
+
+
+
+//нужно сгенерить массив из 3х массивов на 3 раунда [question, rightAnswer]
+const getRounds =()=> {
+    const rounds=[];//задали пустой массив для 3х раундов
+    const n=3;
+    for ( let i=1;i<=n; i++) {
+    const round=[];// свободный массив для одного раунда [question, rightAnswer]
+    let number1= getRandomNumber();
+    let number2=getRandomNumber();
+    let rightAnswer = getRightAnswer(number1, number2);
+    let oper = getOper(rightAnswer, number1, number2);
     
-    
-    //получения мат операции для вопроса игра Калькулятор
-        let oper;
-         if (rightAnswer / number2 === number1) {
-            oper ='*';
-         }
-         if (rightAnswer-number2 === number1) {
-           oper ='+';
-         }
-         if (rightAnswer+number2 === number1) {
-            oper='-';
-         }
     const question =(` ${number1} ${oper} ${number2}`);// интерполяция вопроса
     
     round[0]=question;
